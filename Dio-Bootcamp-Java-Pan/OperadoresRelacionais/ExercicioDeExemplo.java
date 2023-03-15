faa96507511f8814a9a08cd3103b8419f70d1c4e
sergio <sergiofatec@gmail.com>
2023-03-14 23:07:15 -0300

    Atualização do dia. Estudos de controle de fluxo.

---
+++ Dio-Bootcamp-Java-Pan/OperadoresRelacionais/ExercicioDeExemplo.java
@@ -0,0 +1,39 @@
+package OperadoresRelacionais;
+
+public class ExercicioDeExemplo {
+	public static void main(String[] args) {
+		
+		int i1 = 10;
+		int i2 = 20;
+		
+		float f1 = 4.5f;
+		float f2 = 3.5f;
+		
+		char c1 = 'x';
+		char c2 = 'y';
+		
+		String s1 = "Fulano";
+		String s2 = "Fulano";
+		
+		boolean b1 = true;
+		boolean b2 = false;
+		
+		long l1 = 1597L;
+		long l2 = 8997L;
+		
+		byte y1 = 1;
+		short h1 = 25;
+		
+		System.out.println("b1 == b2" + (b1 == b2));
+		System.out.println(b1 != b2);
+		//System.out.println(b1 > b2);
+		//System.out.println(b1 <= b2);
+		
+		System.out.println(f1 == f2);
+		System.out.println(f1 != f2);
+		System.out.println(f1 > f2);
+		System.out.println(f1 <= f2);
+		
+		//termina r 
+	}
+}
