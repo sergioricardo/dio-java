9dc1593f455c29c7d5d099721b4bf2d5ba248acf
sergio <sergiofatec@gmail.com>
2023-03-15 22:54:09 -0300

    Atualizando classe ControleDeFluxo.

--- Dio-Bootcamp-Java-Pan/controle_de_fluxo/ControleDeFluxo.java
+++ Dio-Bootcamp-Java-Pan/controle_de_fluxo/ControleDeFluxo.java
@@ -1,5 +1,26 @@
 package controle_de_fluxo;
 
 public class ControleDeFluxo {
-
+	public static void main(String[] args) {
+		
+		ifFlecha();
+		ifSemFlecha();
+		ifFerias();
+		ifMenor();
+		
+		switchSemana();
+		switchNumero();
+		switchFerias();
+		
+	}
+	
+	private static void ifFlecha() {
+		// evitar o efeito Flecha
+		int mes = 9;
+		if (mes == 1) {
+			
+		}
+	}
+	
+	
 }
